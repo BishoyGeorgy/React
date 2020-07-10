d0eae151b93d6e9a794a00f13e5b95a556263f13
BishoyGeorgy <bishoy.georgy93@gmail.com>
2020-07-11 01:11:26 +0200

    [Adopt-Me] Add useEffect and calling the pet API to render the data

--- Adopt-Me/src/SearchParams.js
+++ Adopt-Me/src/SearchParams.js
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
-import useDropdown from './useDropdown';
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA");
-    const [breeds, setBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, Breedropdown] = useDropdown("Breed", "", breeds);
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, Breedropdown, setBreed] = useDropdown("Breed", "", breeds);
 
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    Location
-                    <h1>{location}</h1>
-                    <input id="location"
-                        value={location}
-                        placeholder="Location"
-                        onChange={e => setLocation(e.target.value)} />
-                </label>
-                <AnimalDropdown />
-                <Breedropdown />
-                <button>Submit</button>
-            </form>
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
 
-        </div>
-    )
-}
+    pet.breeds(animal).then(({ breeds }) => {
+      const breeString = breeds.map(({ name }) => name);
+      setBreeds(breeString);
+    }, console.error);
+  }, [animal, setBreed]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <h1>{location}</h1>
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <Breedropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default SearchParams;
